f50a9f29eecb6fa57f32ad18f2950ef95ee5e901
Abhishek Vishwakarma <surendravishwakarma99887@gmail.comm>
2025-02-25 08:52:49 +0000

    first commit

---
+++ leetcodedirectaccessbyqr.py
@@ -0,0 +1,4 @@
+import qrcode as qr
+img=qr.make("https://leetcode.com/")
+img.save("leetcode.png")
+
--- main.py
+++
@@ -1,3 +0,0 @@
-import qrcode as qr
-img=qr.make("https://sourceforge.net/projects/mingw/")
-img.save("Project-00.png")
